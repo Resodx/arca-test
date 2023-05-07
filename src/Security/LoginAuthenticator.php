8f92901de0a93baf212dcecaa5dff687323be9c0
Helyederson Naves <helyedersonnaves@hotmail.com>
2023-05-07 19:27:05 -0300

    feat: add categories entity

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -49,7 +49,7 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
 
         // For example:
         // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        return new RedirectResponse($this->urlGenerator->generate('app_login'));
+        return new RedirectResponse($this->urlGenerator->generate('app_home'));
     }
 
     protected function getLoginUrl(Request $request): string
